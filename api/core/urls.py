9b1412408ac2a5fbc1a5e437b21c10554a1d7590
Michael Tillman <mnamllit@gmail.com>
2022-07-03 12:23:17 -0500

    Added feedback detail view and url conf.

--- api/core/urls.py
+++ api/core/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('', views.api_root),
 
     path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
 
     path('feedback/', views.FeedbackList.as_view(), name='feedback_list'),
+    path('feedback/<int:pk>/', views.FeedbackDetail.as_view(), name='feedback_detail'),
 ]
 
 
--- api/core/views.py
+++ api/core/views.py
@@ -70,8 +70,8 @@ class ProductDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
+        product = self.get_object(pk)
+        product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class FeedbackList(APIView):
@@ -92,3 +92,38 @@ class FeedbackList(APIView):
             print(serializer)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FeedbackDetail(APIView):
+    """
+    GET, PUT, and DELETE instances of Feedback.
+    """
+
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+        ]
+
+    def get_object(self, pk):
+        try:
+            return Feedback.objects.get(pk=pk)
+        except Feedback.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        feedback = self.get_object(pk)
+        serializer = FeedbackSerializer(feedback)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        feedback = self.get_object(pk)
+        serializer = FeedbackSerializer(feedback, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        feedback = self.get_object(pk)
+        feedback.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
